8d599047738cce5b221568f8280c525e5e3c657e
System Administrator <admin@example.com>
2025-04-12 00:15:39 +0200

    t

--- src/server/json_tools.js
+++ src/server/json_tools.js
@@ -2,6 +2,5 @@
 import { Draft04, Draft06, Draft07, Draft2019, Draft, JsonError } from "json-schema-library";
 //https://github.com/aspecto-io/genson-js
 import { createSchema } from 'genson-js';
-
 export const json_schema_library = { Draft04, Draft06, Draft07, Draft2019, Draft, JsonError };
 export { createSchema } 
