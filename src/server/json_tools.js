6a8cefd201c7091a32673cc47cc88bc3627e0450
System Administrator <admin@example.com>
2025-04-12 00:33:29 +0200

    d

--- src/server/json_tools.js
+++ src/server/json_tools.js
@@ -1,6 +1,6 @@
 //https://github.com/sagold/json-schema-library
 import { Draft04, Draft06, Draft07, Draft2019, Draft, JsonError } from "json-schema-library";
-//https://github.com/aspecto-io/genson-js
+//https://github.com/aspecto-io/genson-js. Used t create schemas from jsons
 import { createSchema } from 'genson-js';
 export const json_schema_library = { Draft04, Draft06, Draft07, Draft2019, Draft, JsonError };
 export { createSchema } 
